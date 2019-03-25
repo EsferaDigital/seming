fcdc00ae8e25dc0a271dedc4af86e377b7057f51
Gabriel Zavando <gabrielzavando@gmail.com>
2019-03-25 12:52:19 -0300

    Modifique los breackpoint del responsive, añadi imagen y texto al mapa en movil, mejore el tamaño de contacto rrss y añadi los link a linkedin y habitissimo

--- src/js/dev/mapa.js
+++ src/js/dev/mapa.js
@@ -6,7 +6,7 @@ const mapa = function mapaResponsive() {
       document.getElementById('Mapa').innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3333.874883497583!2d-70.73711308520318!3d-33.32208629832109!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662bf5b7898c66b%3A0x5ea06f171825fbe2!2sLas+Encinas+1392%2C+Lampa%2C+Regi%C3%B3n+Metropolitana!5e0!3m2!1ses-419!2scl!4v1553446358952" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'
     }
     else{
-      document.getElementById('Mapa').innerHTML = '<a href="https://goo.gl/maps/J8zQKJXwkMR2" target="_blank"><img src="img/maps.jpg" alt="Procesos"></a><p>Ver en Google Maps</p>'
+      document.getElementById('Mapa').innerHTML = '<a href="https://goo.gl/maps/J8zQKJXwkMR2" target="_blank"><span class="Link-mapa">Ver en Google Maps</span><img src="img/maps.jpg" alt="Mapa"></a>'
     }
   }
 
