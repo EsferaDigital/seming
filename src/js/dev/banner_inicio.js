e53768cdd8df685739e0a6eb74448852e7421c7f
Gabriel Zavando <gabrielzavando@gmail.com>
2019-03-26 22:20:28 -0300

    Termine la maquetacion completa de la pagina de inicio

--- src/js/dev/banner_inicio.js
+++ src/js/dev/banner_inicio.js
@@ -7,6 +7,14 @@ const BannerInicio = function Banner(){
       <div class="Banner-inicio-text">
         <p>INTEGRIDAD - EXCELENCIA - CONFIANZA - EFICIENCIA</p>
       </div>`
+      document.getElementById('obrasInicioBanner').innerHTML = `<div class="Obras-inicio-banner-img"><img src="img/banner-inicio-obras.jpg" alt="Construccion Obreros"></div>
+      <div class="Obras-inicio-banner-text">
+        <h2>+ 500.000<span>m&#178</span></h2>
+        <div class="Obras-inicio-banner-text-parrafo">
+          <p>DE GALPONES Y OBRAS INDUSTRIALES</p>
+          <p>A LO LARGO DE CHILE</p>
+        </div>
+      </div>`
     }
     else{
       document.getElementById('BannerInicio').innerHTML = `<div class="Banner-inicio-img"><img src="img/banner-inicio.jpg" alt="Banner-inicio"></div>
@@ -14,6 +22,8 @@ const BannerInicio = function Banner(){
         <p>INTEGRIDAD - EXCELENCIA</p>
         <p>CONFIANZA - EFICIENCIA</p>
       </div>`
+
+      document.getElementById('obrasInicioBanner').innerHTML = ''
     }
   }
 
